2c2a6a127a06c86b19cefb93ec9759d2ed4aa752
lannory <detochkanikolay@gmail.com>
2025-03-17 19:15:43 +0200

    hw12.1 fix

--- hw12/task1/script.js
+++ hw12/task1/script.js
@@ -7,7 +7,7 @@ btns.addEventListener('click', (e)=>{
 		userLink = prompt('Enter link you want to redirect'); 
 	}
 	if(target.getAttribute('data-type') === 'redirect'){
-		window.location = `https://${userLink}`;
+		window.location = `https://${userLink || '#'}`;
 	}
 	
 });
