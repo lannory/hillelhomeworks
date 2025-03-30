7bae9a13072c908b991138a2122ffeea8e95243c
lannory <detochkanikolay@gmail.com>
2025-03-30 19:00:26 +0300

    hw 15 was completed

---
+++ hw14/script.js
@@ -0,0 +1,86 @@
+const slider = document.querySelector('.slider'),
+	prev = document.querySelector('.prev-btn'),
+	next = document.querySelector('.next-btn'),
+	sliderItems = document.querySelectorAll('.slider-item'),
+	dotControls = document.querySelector('.slider-dot-controls');
+
+let currIndex = 0;
+
+
+function hideSlides (index){
+	sliderItems.forEach((item, i) =>{
+		item.classList.remove('show');
+		item.classList.add('hide');
+		if(i === index){
+			item.classList.remove('hide');
+			item.classList.add('show');
+
+		}
+	});
+}
+
+function initDots(){
+	for(let i = 0; i< sliderItems.length; i++){
+		dotControls.innerHTML += `<img data-id="${i+1}" class="dot" src="./img/icons8-dot-48.png" alt="">`;
+	}
+}
+
+initDots();
+
+function hideControls(index){
+	if(index === 0){
+		prev.classList.remove('show');
+		prev.classList.add('hide');
+	}
+	else{
+		prev.classList.remove('hide');
+		prev.classList.add('show');
+	}
+
+	if(index === sliderItems.length - 1){
+		next.classList.remove('show');
+		next.classList.add('hide');
+	}else{
+		next.classList.add('show');
+		next.classList.remove('hide');
+	}
+}
+
+function activeDot (index){
+	document.querySelectorAll('.dot').forEach((item, i) =>{
+		item.style.backgroundColor = '';
+
+		if(i == index){
+			item.style.backgroundColor = 'red';
+		}
+
+	})
+}
+
+hideSlides(currIndex);
+hideControls(currIndex);
+activeDot(currIndex);
+
+
+next.addEventListener('click', ()=>{
+	currIndex += 1;
+	hideSlides(currIndex);
+	hideControls(currIndex);
+ 	activeDot(currIndex);
+});
+prev.addEventListener('click' ,()=>{
+	currIndex -= 1;
+	hideSlides(currIndex);
+	hideControls(currIndex);
+	activeDot(currIndex);
+
+})
+
+dotControls.addEventListener('click', (e)=>{
+	const target = e.target;
+	currIndex = target.getAttribute('data-id') - 1;
+	hideSlides(currIndex);
+	hideControls(currIndex);
+	activeDot(currIndex);
+
+});
---
+++ hw15/script.js
@@ -0,0 +1,52 @@
+
+
+
+const todoContainer = document.querySelector('.js--todos-wrapper');
+const addInput = document.forms[0].value, addBtn = document.querySelector('.form__btn');
+let tasks = [];
+
+fillTodo()
+
+function fillTodo () {
+	todoContainer.innerHTML = '';
+	if(tasks.length === 0) 
+		tasks = Array.from(JSON.parse(localStorage.getItem('todo')));
+	tasks.forEach((item, i) =>{
+		todoContainer.innerHTML += `<li class= "${item.isChecked ? "todo-item todo-item--checked" : "todo-item"}" data-id=${i}>
+           <input type="checkbox" data-type="cross">
+           <span class="todo-item__description">${item.value}</span>
+           <button class="todo-item__delete" data-type="delete">Видалити</button>
+       </li>`
+	});
+}
+
+function fillLocalStorage (){
+	localStorage.setItem('todo', JSON.stringify(tasks));
+}
+
+addBtn.addEventListener('click', (e)=>{
+	e.preventDefault();
+	tasks.push({value: addInput.value, isChecked: false});
+	fillLocalStorage();
+	fillTodo();
+	
+});
+
+todoContainer.addEventListener('click', (e)=>{
+
+	const target = e.target, targetId = target.parentNode.getAttribute('data-id');
+
+	if(target.getAttribute('data-type') === 'cross'){
+		tasks[targetId].isChecked = !tasks[targetId].isChecked;
+		fillTodo();
+		fillLocalStorage() 
+	}
+
+	if(target.getAttribute('data-type') === 'delete'){
+		tasks = tasks.filter((item, i) => {return i != targetId});
+		console.log(tasks);
+		fillLocalStorage()
+		fillTodo();
+	}
+});
+
